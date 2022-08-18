35bce72525815fcba0e7b09e443b75da07e3d695
Stacey Brinkley <brinkleystacey12@gmail.com>
2022-08-18 15:25:18 -0400

    created functiond for 3 more winning hands

--- pokerhandList.js
+++ pokerhandList.js
@@ -9,7 +9,7 @@ const hand = [
     'HIGHEST POSSIBLE HAND: TWO PAIR',
     'HIGHEST POSSIBLE HAND: ONE PAIR',
     'HIGHEST POSSIBLE HAND: HIGH CARD'
-  ];
+];
 
 function fourOfAKind(cards) {
     const cardValues = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
@@ -22,7 +22,7 @@ function fourOfAKind(cards) {
 
     // counting values
     handValue.forEach((ele, i) => {
-        if(handValue[i].length == 4) {
+        if (handValue[i].length == 4) {
             return true
         }
     })
@@ -31,16 +31,67 @@ function fourOfAKind(cards) {
 function flush(cards) {
     let handValue = [];
     var suits = ['CLUBS', 'HEARTS', 'SPADES', 'DIAMONDS'];
-  
-    // Sort suits
+
     suits.forEach((ele, i) => {
         handValue.push(cards.filter((suit) => suit == suits[i]))
-    }) 
-    
-    // Count suits
+    })
+
     handValue.forEach((ele, i) => {
-        if(handValue[i].length == 5) {
+        if (handValue[i].length == 5) {
             return true
         }
     })
-  }
+}
+
+function isThreeOfAKind(cards) {
+    const cardValues = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+    let handValue = [];
+
+    cardValues.forEach((ele, i) => {
+        handValue.push(cards.filter(value => value == cardValues[i]))
+    })
+
+    handValue.forEach((ele, i) => {
+        if (handValue[i].length == 3) {
+            return true
+        }
+    })
+}
+
+function isTwoPair(cards) {
+    const cardValues = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+    let handValue = [];
+    let pairCount = 0;
+
+    cardValues.forEach((ele, i) => {
+        handValue.push(cards.filter(value => value == cardValues[i]))
+    })
+
+    handValue.forEach((ele, i) => {
+        if (handValue[i].length == 2) {
+            pairCount++
+        }
+    })
+
+    if (pairCount == 2)
+        return true;
+}
+
+function isOnePair(cards) {
+    const cardValues = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+    let handValue = [];
+    let pairCount = 0;
+
+    cardValues.forEach((ele, i) => {
+        handValue.push(cards.filter(value => value == cardValues[i]))
+    })
+
+    handValue.forEach((ele, i) => {
+        if (handValue[i].length == 2) {
+            pairCount++
+        }
+    })
+
+    if (pairCount == 1)
+        return true;
+}
